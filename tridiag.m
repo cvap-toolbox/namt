e2d2b68e78c18d292685f54eedbfe1c3d1f060db
MartinHjelm <mar.hjelm@gmail.com>
2014-05-14 11:19:45 +0200

    Added description

--- tridiag.m
+++ tridiag.m
@@ -1,14 +1,30 @@
 
 
 function trid = tridiag (subdiag, maindiag, supdiag)
+% function trid = tridiag (subdiag, maindiag, supdiag)
+% Creates a (N,N) tri-diagonal matrix given sub-, sup- and main-diagonal vectors.
+%
+% usage
+%     trid = tridiag (subdiag, maindiag, supdiag)
+%
+% input
+%     subdiag : Vector of size N-1. 
+%     maindiag : Vector of size N. 
+%     supdiag : Vector of size N-1. 
+%
+% output
+%                  
+% description
+%     Creates a (N,N) tri-diagonal matrix given sub-, sup- and main-diagonal vectors.
+%
+% author
+%     Martin Hjelm, martinhjelm@kth.se  
 
-%   diag(ones(m,1),0)+diag(ones(m-1,1),1)+diag(ones(m-1,1),-1)
-  
   [N,M] = size(maindiag);
   if N == 1
   	N = M;
   end
 
-  trid = diag(maindiag,0)+diag(supdiag(1:N-1),1)+diag(subdiag(1:N-1),-1);
+  trid = diag(maindiag,0) + diag(supdiag(1:N-1),1) + diag(subdiag(1:N-1),-1);
 
 end
